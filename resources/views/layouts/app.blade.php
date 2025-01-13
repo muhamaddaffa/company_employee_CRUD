94d5b86761f964ea169643eba535b1ae60228ec4
rendi14 <daffafa09@gmail.com>
2025-01-13 14:41:47 +0700

    Feat: add logout button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,5 +34,8 @@
         </main>
     </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </html>
