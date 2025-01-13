9dd0df0d77ed5b3156b82de776c2de974c3b2bdf
rendi14 <daffafa09@gmail.com>
2025-01-13 14:52:42 +0700

    Update index.blade.php

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -10,7 +10,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="container">
-                        <h1>Companies</h1>
                         <a href="{{ route('companies.create') }}" class="btn btn-primary">Add Company</a>
                         <table class="table mt-3">
                             <thead>
